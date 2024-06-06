d900d7642b9e1b1041ecf6fa6a92091e8ccc76ba
gs <gilseong.ryu@gmail.com>
2024-06-06 16:49:19 +0900

    logo update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -61,7 +61,7 @@ const config: Config = {
       title: 'IEYEI SITE',
       logo: {
         alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        src: 'img/one-ring.svg',
       },
       items: [
         {
